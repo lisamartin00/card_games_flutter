0b3900580b02512fa7d85d0ac8097bfffad3f525
Lisa Martin <lisamartin00@gmail.com>
2020-11-23 16:56:53 -0700

    add card game card list

--- lib/main.dart
+++ lib/main.dart
@@ -5,109 +5,359 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Card Games',
+      home: CardGameList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CardGameList extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _CardGameListState createState() => _CardGameListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _CardGameListState extends State<CardGameList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card Games'),
+      ),
+      body: _buildCards(),
+    );
   }
+
+  Widget _buildCards() {
+    return Scrollbar(
+      child: ListView(
+          padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+          children: [
+            for (final game in games(context))
+              Container(
+                margin: const EdgeInsets.only(bottom: 8),
+                child: CardGameItem(game: game),
+              ),
+          ]),
+    );
+  }
+}
+
+class CardGame {
+  const CardGame({
+    @required this.name,
+    @required this.imgSrc,
+    @required this.minAge,
+    @required this.howToPlayUrl,
+    @required this.requiresSpecialDeck,
+    @required this.numPlayers,
+  })  : assert(name != null),
+        assert(imgSrc != null),
+        assert(minAge != null),
+        assert(howToPlayUrl != null),
+        assert(requiresSpecialDeck != null),
+        assert(numPlayers != null);
+
+  final String name;
+  final String imgSrc;
+  final int minAge;
+  final String howToPlayUrl;
+  final bool requiresSpecialDeck;
+  final String numPlayers;
+}
+
+List<CardGame> games(BuildContext context) => [
+      CardGame(
+        name: 'California Speed',
+        imgSrc: 'assets/californiaSpeed.jpg',
+        minAge: 7,
+        howToPlayUrl:
+            'https://www.itsalwaysautumn.com/play-california-speed-easy-card-game.html',
+        requiresSpecialDeck: false,
+        numPlayers: '2',
+      ),
+      CardGame(
+        name: 'Canasta',
+        imgSrc: 'assets/canasta.png',
+        minAge: 12,
+        howToPlayUrl: 'https://bicyclecards.com/how-to-play/canasta/',
+        requiresSpecialDeck: false,
+        numPlayers: '4',
+      ),
+      CardGame(
+        name: 'Egyptian Rat Slap',
+        imgSrc: 'assets/ers.jpg',
+        minAge: 7,
+        howToPlayUrl:
+            'https://bicyclecards.com/how-to-play/egyptian-rat-screw/',
+        requiresSpecialDeck: false,
+        numPlayers: '2+',
+      ),
+      CardGame(
+        name: 'FreeCell',
+        imgSrc: 'assets/freeCell.jpg',
+        minAge: 12,
+        howToPlayUrl: 'http://www.solitairecity.com/Help/FreeCell.shtml',
+        requiresSpecialDeck: false,
+        numPlayers: '1',
+      ),
+      CardGame(
+        name: 'Mau',
+        imgSrc: 'assets/mau.jpeg',
+        minAge: 8,
+        howToPlayUrl: 'https://en.wikipedia.org/wiki/Mao_(card_game)',
+        requiresSpecialDeck: false,
+        numPlayers: '3+',
+      ),
+      CardGame(
+        name: 'Nerts',
+        imgSrc: 'assets/nerts.jpg',
+        minAge: 12,
+        howToPlayUrl: 'https://bicyclecards.com/how-to-play/nerts/',
+        requiresSpecialDeck: false,
+        numPlayers: '2+',
+      ),
+      CardGame(
+        name: 'Phase 10',
+        imgSrc: 'assets/phase10.jpg',
+        minAge: 7,
+        howToPlayUrl: 'https://www.instructables.com/How-to-Play-Phase-10/',
+        requiresSpecialDeck: true,
+        numPlayers: '2+',
+      ),
+      CardGame(
+        name: 'Set',
+        imgSrc: 'assets/set.jpg',
+        minAge: 8,
+        howToPlayUrl: 'https://www.setgame.com/set/puzzle_rules',
+        requiresSpecialDeck: true,
+        numPlayers: '1+',
+      ),
+      CardGame(
+        name: 'SkipBo',
+        imgSrc: 'assets/skipbo.jpg',
+        minAge: 8,
+        howToPlayUrl: 'https://service.mattel.com/instruction_sheets/42050.pdf',
+        requiresSpecialDeck: true,
+        numPlayers: '2+',
+      ),
+      CardGame(
+        name: 'Sleeping Queens',
+        imgSrc: 'assets/sleepingQueens.jpg',
+        minAge: 5,
+        howToPlayUrl:
+            'https://www.fgbradleys.com/rules/rules4/Sleeping%20Queens%20-%20rules.pdf',
+        requiresSpecialDeck: true,
+        numPlayers: '2+',
+      ),
+      CardGame(
+        name: 'Speed',
+        imgSrc: 'assets/speed.jpg',
+        minAge: 8,
+        howToPlayUrl:
+            'https://www.instructables.com/How-to-play-speed-card-game/',
+        requiresSpecialDeck: false,
+        numPlayers: '2',
+      ),
+      CardGame(
+        name: 'Spit',
+        imgSrc: 'assets/spit.jpg',
+        minAge: 8,
+        howToPlayUrl: 'https://bicyclecards.com/how-to-play/spit/',
+        requiresSpecialDeck: false,
+        numPlayers: '2',
+      ),
+      CardGame(
+        name: 'Solitaire',
+        imgSrc: 'assets/solitaire.png',
+        minAge: 12,
+        howToPlayUrl: 'https://bicyclecards.com/how-to-play/solitaire/',
+        requiresSpecialDeck: false,
+        numPlayers: '1',
+      ),
+      CardGame(
+        name: 'Spoons',
+        imgSrc: 'assets/spoons.png',
+        minAge: 10,
+        howToPlayUrl: 'https://bicyclecards.com/how-to-play/spoons/',
+        requiresSpecialDeck: false,
+        numPlayers: '3+',
+      ),
+      CardGame(
+        name: 'War',
+        imgSrc: 'assets/war.jpg',
+        minAge: 5,
+        howToPlayUrl: 'https://bicyclecards.com/how-to-play/war/',
+        requiresSpecialDeck: false,
+        numPlayers: '2+',
+      ),
+    ];
+
+class CardGameItem extends StatelessWidget {
+  const CardGameItem({Key key, @required this.game, this.shape})
+      : assert(game != null),
+        super(key: key);
+
+  // This height will allow for all the Card's content to fit comfortably within the card.
+  static const height = 270.0;
+  final CardGame game;
+  final ShapeBorder shape;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          children: [
+            SizedBox(
+              height: height,
+              child: Card(
+                // This ensures that the Card's children (including the ink splash) are clipped correctly.
+                clipBehavior: Clip.antiAlias,
+                shape: shape,
+                child: InkWell(
+                  onTap: () {
+                    print('Card was tapped');
+                  },
+                  // Generally, material cards use onSurface with 12% opacity for the pressed state.
+                  splashColor:
+                      Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
+                  // Generally, material cards do not have a highlight overlay.
+                  highlightColor: Colors.transparent,
+                  child: CardGameContent(game: game),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(4, 4, 4, 12),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(title, style: Theme.of(context).textTheme.subtitle1),
+      ),
+    );
+  }
+}
+
+class CardGameContent extends StatelessWidget {
+  const CardGameContent({Key key, @required this.game})
+      : assert(game != null),
+        super(key: key);
+
+  final CardGame game;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final descriptionStyle = theme.textTheme.subtitle1;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 184,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Image(
+                  image: AssetImage(game.imgSrc),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // Description and share/explore buttons.
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: DefaultTextStyle(
+            softWrap: false,
+            overflow: TextOverflow.ellipsis,
+            style: descriptionStyle,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // This array contains the three line description on each card
+                // demo.
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(game.name),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Number of Players: ${game.numPlayers}',
+                    style: descriptionStyle.copyWith(color: Colors.black54),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// return Scaffold(
+//   appBar: AppBar(
+//     // Here we take the value from the MyHomePage object that was created by
+//     // the App.build method, and use it to set our appbar title.
+//     title: Text(widget.title),
+//   ),
+//   body: Center(
+//     // Center is a layout widget. It takes a single child and positions it
+//     // in the middle of the parent.
+//     child: Column(
+//       // Column is also a layout widget. It takes a list of children and
+//       // arranges them vertically. By default, it sizes itself to fit its
+//       // children horizontally, and tries to be as tall as its parent.
+//       //
+//       // Invoke "debug painting" (press "p" in the console, choose the
+//       // "Toggle Debug Paint" action from the Flutter Inspector in Android
+//       // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+//       // to see the wireframe for each widget.
+//       //
+//       // Column has various properties to control how it sizes itself and
+//       // how it positions its children. Here we use mainAxisAlignment to
+//       // center the children vertically; the main axis here is the vertical
+//       // axis because Columns are vertical (the cross axis would be
+//       // horizontal).
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         Text(
+//           'You have pushed the button this many times:',
+//         ),
+//         Text(
+//           '$_counter',
+//           style: Theme.of(context).textTheme.headline4,
+//         ),
+//       ],
+//     ),
+//   ),
+//   floatingActionButton: FloatingActionButton(
+//     onPressed: _incrementCounter,
+//     tooltip: 'Increment',
+//     child: Icon(Icons.add),
+//   ), // This trailing comma makes auto-formatting nicer for build methods.
